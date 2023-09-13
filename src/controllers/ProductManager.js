0dc1e7ca091ae2eb70f8b9b3beee3b6b29f84c23
Maxfuente <max.fuentes.a@gmail.com>
2023-09-13 00:04:53 -0300

    add post

--- src/controllers/ProductManager.js
+++ src/controllers/ProductManager.js
@@ -16,7 +16,6 @@ writeProducts = async (product) => {
     await fs.writeFile(this.path, JSON.stringify(product));
 };
 
-
 addProducts = async (product) =>{
 let productsOld = await this.readProducts()  
 product.id = nanoid() 
@@ -27,6 +26,24 @@ return "Producto Agregado Exitosamente"
 
 getProducts = async ()=>{
 return await this.readProducts()
+};
+
+getProductsById = async (id)=>{
+let products = await this.readProducts()
+let productById = products.find(prod => prod.id === id)
+if (!productById) return " No esta el producto"
+return productById
+};
+
+deleteProducts = async (id)=>{
+    let products = await this.readProducts();
+    let existProducts = products.some(prod => prod.id === id)
+    if (existProducts){
+    let filterProducts = products.filter(prod => prod.id != id)
+    await   this.writeProducts(filterProducts)
+    return "producto borrado"
+    }   
+    return "producto que se desea borrar no existe"
 }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express, { request, response } from "express";
 import ProductManager from "./controllers/ProductManager.js";
 import res from "express/lib/response.js";
+import { parse } from "path";
+import req from "express/lib/request.js";
 
 const product = new ProductManager();
 
@@ -13,11 +15,22 @@ app.use(express.urlencoded({extended: true}));
 app.get("/products", async (request,response)=>{
     response.send (await product.getProducts())
 })
+app.get("/products/:id", async (request,response)=>{
+    let id =  request.params.id
+    response.send (await product.getProductsById(id))
+})
+
+
 
 app.post("/products", async (request,response)=>{
    let newProduct = request.body
    response.send (await product.addProducts(newProduct))
-})
+});
+
+app.delete("/products/:id", async (request,response)=>{
+    let id = request.params.id
+    response.send (await product.deleteProducts(id))
+});
 
 app.listen(PORT, ()=>{
     console.log(`Servidor PortEXpress ${PORT}`);
